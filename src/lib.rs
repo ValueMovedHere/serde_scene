8b257491a8f25a1ab5190eaa213d03d208e8da6d
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-03 20:09:57 +0800

    Read rotation data from json

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use serde::Deserialize;
 use serde_json::from_reader;
 
 pub fn from_json(path: &str) -> Vec<(Vec3, Quaternion, Collider)> {
-    let file = File::open(path).expect("Failed to read json");
+    let file = File::open(path).expect("Failed to read colliders from json");
     let reader = BufReader::new(file);
     let colliders_raw: Vec<Data> = from_reader(reader).expect("Failed to parse json");
     let mut colliders_vec: Vec<(Vec3, Quaternion, Collider)> = Vec::new();
@@ -21,7 +21,12 @@ pub fn from_json(path: &str) -> Vec<(Vec3, Quaternion, Collider)> {
                 collider.transform.1,
                 collider.transform.2,
             ),
-            Quaternion::IDENTITY,
+            Quaternion::from_xyzw(
+                collider.rotation.0,
+                collider.rotation.1,
+                collider.rotation.2,
+                0f32,
+            ),
             Collider::cuboid(collider.scale.0, collider.scale.1, collider.scale.2),
         );
         colliders_vec.push(collider_tuple);
@@ -33,5 +38,6 @@ pub fn from_json(path: &str) -> Vec<(Vec3, Quaternion, Collider)> {
 struct Data {
     #[serde(rename = "position")]
     transform: (f32, f32, f32),
+    rotation: (f32, f32, f32),
     scale: (f32, f32, f32),
 }
