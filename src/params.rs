387f0b719ca448b8d14a276cbf6a8803b9d644c1
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-05 00:14:54 +0800

    Make the fields of params public to the crate

--- src/params.rs
+++ src/params.rs
@@ -2,30 +2,30 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct CuboidParams {
-    width: f32,
-    height: f32,
-    depth: f32,
+    pub(crate) width: f32,
+    pub(crate) height: f32,
+    pub(crate) depth: f32,
 }
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct SphereParams {
-    radius: f32,
+    pub(crate) radius: f32,
 }
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct CylinderParams {
-    radius: f32,
-    height: f32,
+    pub(crate) radius: f32,
+    pub(crate) height: f32,
 }
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct ConeParams {
-    radius: f32,
-    height: f32,
+    pub(crate) radius: f32,
+    pub(crate) height: f32,
 }
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct CapsuleParams {
-    radius: f32,
-    length: f32,
+    pub(crate) radius: f32,
+    pub(crate) length: f32,
 }
