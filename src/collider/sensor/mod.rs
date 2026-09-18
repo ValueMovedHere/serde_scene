7fd9914a6cbe442a0b4d78f5c24601ef2883956e
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-18 22:38:39 +0800

    modified:   src/collider/sensor/mod.rs

--- src/collider/sensor/mod.rs
+++ src/collider/sensor/mod.rs
@@ -10,6 +10,9 @@ use bevy::prelude::Vec3;
 use serde::Deserialize;
 use serde_json::from_reader;
 
-use crate::collider::{self, ColliderData};
+use crate::collider::{self, ColliderData, a_collider_from};
 
-pub fn from_json(path: &str) -> (RigidBody, Collider, Sensor) {}
+pub fn from_json(path: &str) -> (RigidBody, Collider, Sensor) {
+    let collider = a_collider_from(path);
+    (RigidBody::Static, collider, Sensor)
+}
