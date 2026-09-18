81213729cc00a70dc785352c7cfcc8864a662e86
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-18 15:34:57 +0800

    modified:   src/collider/sensor/mod.rs

--- src/collider/sensor/mod.rs
+++ src/collider/sensor/mod.rs
@@ -1,5 +1,16 @@
-use serde_json::from_reader;
+use std::fs::Fils;
+use std::io::BufReader;
 
-use avian3d::prelude::*;
+use avian3d::{
+    math::Quaternion, //
+    prelude::{
+        Collider, //
+        RigidBody,
+        Sensor,
+    },
+};
+use bevy::prelude::Vec3;
+use serde::Deserialize;
+use serde_json::from_reader;
 
 pub fn from_json(path: &str) -> (RigidBody, Collider, Sensor) {}
