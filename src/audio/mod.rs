ad62658d9445ab302cb0d7fd0031cd0b7a3aa129
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-25 22:50:44 +0800

    modified:   src/audio/mod.rs

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -2,7 +2,24 @@ use std::fs::File;
 use std::io::BufReader;
 
 use bevy::prelude::*;
+use serde_json;
 
-use crate::collider::ShapeType;
-
-pub fn from_json(path: &str) -> (Transform, AudioPlayer, PlaybackSettings) {}
+pub fn from_json(
+    path: &str,
+    audio_player: AudioPlayer,
+    playback_settings: PlaybackSettings,
+) -> Vec<(Transform, AudioPlayer, PlaybackSettings)> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    let pos_data_vec: Vec<(f32, f32, f32)> = serde_json::from_reader(reader).unwrap();
+    let mut sound_data_vec = Vec::with_capacity(1usize);
+    // 假设这些位置对应的是同一个音频, 且具有相同的播放设置
+    for item in pos_data_vec {
+        sound_data_vec.push((
+            Transform::from_xyz(item.0, item.1, item.2),
+            audio_player.clone(),
+            playback_settings,
+        ));
+    }
+    sound_data_vec
+}
